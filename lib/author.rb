826fb8012073a09e2b680c65f379324d7b0d27aa
Julianne Kim <juliannne@gmail.com>
2017-10-22 03:00:55 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
   attr_accessor :name, :posts
-
+  @@post_count = 0
+  
   def initialize(name)
     @name = name
     @posts = []
@@ -8,7 +9,7 @@ class Author
 
   def add_post(note)
     new_post = Post.new(note)
-    new_post.author = self 
+    new_post.author = self
     @posts << new_post
   end
 
