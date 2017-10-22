26c9e3cda92cfbade82b0b8dec12d955732c0c4a
Julianne Kim <juliannne@gmail.com>
2017-10-22 03:03:21 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -6,10 +6,11 @@ class Post
   end
 
   def author_name
-    if self.author == nil
-      return nil
-    else
-      self.author.name
-    end 
+    self.author ? self.author.name : nil 
+    # if self.author == nil
+    #   return nil
+    # else
+    #   self.author.name
+    # end 
   end
 end
