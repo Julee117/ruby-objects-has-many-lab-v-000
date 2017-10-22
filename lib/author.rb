db3cce9db977add15340f296e869de200ed1911a
Julianne Kim <juliannne@gmail.com>
2017-10-22 03:23:09 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,13 +10,14 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     new_post = Post.new(title)
-    @@post_count += title.count
     @posts << new_post
     new_post.author = self
+    @@post_count += 1
   end
 
   def self.post_count
